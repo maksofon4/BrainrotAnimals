0d1c77a4314bb4057a84b3c52918beb1376b4f9e
maksofon4 <skamaneg@gmail.com>
2025-04-20 21:55:48 +0300

    changed the navbar logo text

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -48,7 +48,7 @@ export default function HeroUINavbar() {
     >
       <NavbarBrand>
         <AcmeLogo />
-        <p className="font-bold text-inherit">ACME</p>
+        <p className="font-bold text-inherit">MEMES</p>
       </NavbarBrand>
       <NavbarContent className="hidden sm:flex gap-4" justify="center">
         <NavbarItem isActive={activeLink === "/"}>
